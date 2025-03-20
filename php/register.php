ef7a97379b8a6b996d22ce04813f32640f4e5d7a
Joshua Gray <166338553+ItsYoish@users.noreply.github.com>
2025-03-20 19:22:57 +0000

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,1 +1,54 @@
+<?php
 
+include 'php/db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "Post") {
+
+    $firstName = trim($_POST['first_name']);
+    $lastName - trim($_POST['last_name']);
+    $email = trim($_POST['email']);
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+
+
+    if (empty($firstName) || empty($lastName) || empty($email) || empty($username) || empty($password) || empty($confirmPassword)) {
+        die("Please complete all fields");
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die("Email not valid");
+    }
+
+    if($password !== $confirmPassword) {
+        die("Passwords do not match");
+    }
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $checkIfExists = "SELECT id FROM Table_Name WHERE username = ? OR email = ?";
+    $stmt = $conn->prepare($checkIfExists);
+    $stmt->bind_param("ss", $username, $email);
+    $stmt->excute();
+    $stmt->store_result();
+
+    if($stmt->num_rows > 0) { 
+        die("The username or email already exists");
+    }
+    $stmt->close();
+
+    $sql = "INSERT INTO Table_Name (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->blind_param("sssss", $firstName, $lastName, $email, $username, $hashedPassword);
+
+    if ($stmt->execute()) {
+        echo "Account Created!";
+    } else {
+        echo "Error creating account" . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+}
+?>
